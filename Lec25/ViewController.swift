29f1c25d2e52b46c0c91103c9b890f5e0e079000
Netanel <netanel77787@gmail.com>
2022-01-15 15:34:01 +0200

    getting started

---
+++ Lec25/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lec25
+//
+//  Created by נתנאל מנצור on 15/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
